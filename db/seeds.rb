922c60dd9959aa4816cf326476ac03e035bd09b9
wedesignstudios <deelden@gmail.com>
2016-10-16 13:07:23 -0500

    updated seed file with genres, seed_genres.csv added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 require 'csv'
 
-csv_text = File.read('seed_words.csv')
-csv = CSV.parse(csv_text, headers: true)
+# Seed Words
+csv_words = File.read('seed_words.csv')
+csv_word_seeds = CSV.parse(csv_words, headers: true)
 
-csv.each do |row|
+csv_word_seeds.each do |row|
   row.to_hash
   w = Word.new
   w.string = row['string']
@@ -12,3 +13,17 @@ csv.each do |row|
 end
 
 puts "#{Word.count} new words created in Words table."
+
+# Seed Genres
+csv_genres = File.read('seed_genres.csv')
+csv_genre_seeds = CSV.parse(csv_genres, headers: true)
+
+csv_genre_seeds.each do |row|
+  row.to_hash
+  g = Genre.new
+  g.name = row['name']
+  g.save
+  puts "#{g.name} saved"
+end
+
+puts "#{Genre.count} new genres created in Genres table."
