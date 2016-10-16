f246c6225eabd8e12a037e0d3c3bdc662cb087ce
wedesignstudios <deelden@gmail.com>
2016-10-16 16:37:38 -0500

    add 'new' method

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,11 @@
-class WordsController < ApplicationController
+class WordsController < ApplicationController  
 
   def index
     @words = Word.all
   end
+
+  def new
+    @word = Word.new
+  end  
 
 end
