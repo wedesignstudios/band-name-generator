015dd0f79e1ba885716a097696ea145b4d26940e
wedesignstudios <deelden@gmail.com>
2016-10-16 12:05:09 -0500

    seed file coded, seed_words.csv added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read('seed_words.csv')
+csv = CSV.parse(csv_text, headers: true)
+
+csv.each do |row|
+  row.to_hash
+  w = Word.new
+  w.string = row['string']
+  w.save
+  puts "#{w.string} saved"
+end
+
+puts "#{Word.count} new words created in Words table."
