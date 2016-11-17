0241a8f1b7b17d8d47ebdf69c36833b92ccb244b
wedesignstudios <deelden@gmail.com>
2016-11-16 18:43:15 -0600

    add json format to words index

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,6 +3,11 @@ class WordsController < ApplicationController
 
   def index
     @words = Word.all
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @words}
+    end 
   end
 
   def new
