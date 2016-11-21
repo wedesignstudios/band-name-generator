f5565bb1c4e70c802fba8c825a8dc8e989938a60
wedesignstudios <deelden@gmail.com>
2016-11-20 19:30:04 -0600

    add index '/' state

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,2 +1,10 @@
 angular
-  .module('app', ['ui.router']);
+  .module('app', ['ui.router', 'templates'])
+  .config(function($stateProvider) {      
+      $stateProvider
+          .state('index', {
+              url: '/',
+              templateUrl: 'app/views/index.html',
+              controller: 'IndexController as vm_index'              
+          });      
+  });
