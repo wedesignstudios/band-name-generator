2a6324f9e1add042ccc02dd3c22b369c19afe897
wedesignstudios <deelden@gmail.com>
2016-11-21 18:38:38 -0600

    create initial getBandName() method

--- app/assets/javascripts/app/controllers/IndexController.js
+++ app/assets/javascripts/app/controllers/IndexController.js
@@ -1,4 +1,25 @@
-function IndexController() {  
+function IndexController(WordService) {
+  var ctrl = this;
+
+  ctrl.bandWords = [];
+  ctrl.bandName = '';
+
+  ctrl.getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  ctrl.getBandName = function(numOfWords) {
+    WordService.getWords()
+      .then(function(response) {
+        var i = 0
+        while (i++ < numOfWords) {
+          var randomWordIndex = ctrl.getRandomInt(0, response.data.words.length);
+          ctrl.bandWords.push(response.data.words[randomWordIndex].string);
+        }        
+        ctrl.bandName = ctrl.bandWords.join(' ');
+        ctrl.bandWords = [];
+      });
+  }
 
 }
 
