dd71eeb84618815bd5bfc01b4151a635e48854e1
wedesignstudios <deelden@gmail.com>
2016-11-26 19:45:22 -0600

    add :index, :show, :create, :band_name_params methods

--- app/controllers/band_name_controller.rb
+++ app/controllers/band_name_controller.rb
@@ -1,2 +1,24 @@
 class BandNameController < ApplicationController
+
+  def index
+    band_names = BandName.all
+    render json: band_names
+  end
+
+  def show
+    band_name = BandName.find(params[:id])
+    render json: band_name
+  end
+
+  def create
+    band_name = BandName.create(band_name_params)
+    redirect_to band_name_path(band_name)
+  end
+
+  private
+
+  def band_name_params
+    params.require(:band_name).permit(:name, :genre_id)
+  end
+
 end
