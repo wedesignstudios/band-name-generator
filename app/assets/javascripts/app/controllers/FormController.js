c49f35260788130c6a37a5e12a7fe76350a81458
wedesignstudios <deelden@gmail.com>
2016-12-04 14:34:34 -0600

    add services, getGenreNames() to FormController

--- app/assets/javascripts/app/controllers/FormController.js
+++ app/assets/javascripts/app/controllers/FormController.js
@@ -1,5 +1,16 @@
-function FormController() {
+function FormController($filter, WordService, GenreService, BandNameService) {
+  var ctrl = this;
 
+  ctrl.genres = [];  
+
+  ctrl.getGenreNames = function() {
+    GenreService.getGenres()
+      .then(function(response) {        
+        ctrl.genres.push(response.data.genres);
+      });
+  }
+
+  ctrl.getGenreNames();
 }
 
 angular
