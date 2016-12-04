bfde0c4851111e1f08070bc27845672f7817a67b
wedesignstudios <deelden@gmail.com>
2016-12-04 13:52:52 -0600

    add routes and views to handle step-by-step form

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -6,5 +6,22 @@ angular
               url: '/',
               templateUrl: 'app/views/index.html',
               controller: 'IndexController as vm_index'              
+          })
+          .state('form', {
+              url: '/name-builder',
+              templateUrl: 'app/views/name-builder.html',
+              controller: 'FormController as vm_form'
+          })
+          .state('form.genre', {
+              url: '/genre',
+              templateUrl: 'app/views/name-builder-genre.html'
+          })
+          .state('form.length', {
+              url: '/length',
+              templateUrl: 'app/views/name-builder-length.html'
+          })
+          .state('form.start-word', {
+              url: '/start-word',
+              templateUrl: 'app/views/name-builder-start-word.html'
           });      
   });
