66e7ac8d3763eafb1e5b27fce23d023f669aa0e7
wedesignstudios <deelden@gmail.com>
2017-01-12 16:39:03 -0600

    add twtrTweet method, slight refactor fbFeed method in SocialMediaShareService

--- app/assets/javascripts/app/services/SocialMediaShareService.js
+++ app/assets/javascripts/app/services/SocialMediaShareService.js
@@ -21,12 +21,17 @@ function SocialMediaShareService($window) {
       $window.FB.ui({
         method: 'feed',                
         link: 'https://banderoo.com',
-        title: `I just used Banderoo to name my band ${bandName}.`,
+        title: 'I just used Banderoo to name my band "' + bandName + '"',
         picture: 'http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg',
         description: 'Naming your band is hard. Banderoo makes it easy. And fun!',
         caption: 'banderoo.com',
       }, function(response){});
     }
+
+    this.twtrTweet = (name) => {
+      return `https:\/\/twitter.com/intent/tweet?text=I+just+used+Banderoo+to+name+my+band+%22${name}%22&hashtags=bands,bandname,banderoo&url=https%3A%2F%2Fbanderoo.com%2F`;
+    }
+
 }
 
 angular
