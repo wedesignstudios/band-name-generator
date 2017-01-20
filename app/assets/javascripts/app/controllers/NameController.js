8a64bc1e1acaf6ebd60600d6d2ffd1e71225f868
wedesignstudios <deelden@gmail.com>
2017-01-20 13:11:45 -0600

    add methods to change font-family depending on genre

--- app/assets/javascripts/app/controllers/NameController.js
+++ app/assets/javascripts/app/controllers/NameController.js
@@ -1,18 +1,37 @@
 function NameController($window, $timeout, ShareBandNameService, FormDataService, SocialMediaShareService) {
   var ctrl = this;
 
+  var fontClasses = [
+    'genre-font-pop',
+    'genre-font-indie',
+    'genre-font-punk',
+    'genre-font-rock',
+    'genre-font-metal',
+    'genre-font-jazz',
+    'genre-font-country'
+  ];
+
+  function getFontClass(i) {
+    return fontClasses[i-1];
+  }
+
   ctrl.loadingPage = true;
   ctrl.bandName = ShareBandNameService;
 
   ctrl.removeClassFromElement = (className, element) => {
     let elem = angular.element(document.querySelector(element));
-    elem.removeClass(className);    
+    elem.removeClass(className);
+  }
+
+  ctrl.addClassToElement = (className, element) => {
+    let elem = angular.element(document.querySelector(element));
+    elem.addClass(className);
   }
 
   ctrl.hideLoadingPage = () => {
     ctrl.removeClassFromElement('bg-color-loading', 'body');
     ctrl.removeClassFromElement('bg-img-loading', '.bg-img');
-    ctrl.loadingPage = false;
+    ctrl.loadingPage = false;    
   }
 
   ctrl.resetBandName = function() {
@@ -30,7 +49,9 @@ function NameController($window, $timeout, ShareBandNameService, FormDataService
 
   SocialMediaShareService.fbSDK;
 
-  $timeout(ctrl.hideLoadingPage, 3000);   
+  $timeout(ctrl.hideLoadingPage, 3000).then( () => {
+    ctrl.addClassToElement(getFontClass(ctrl.bandName.genre_id), 'h1#band-name')
+  });
   
 }
 
