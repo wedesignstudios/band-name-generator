5e7907889ff01a71f394f13f72d4be8996ae2398
wedesignstudios <deelden@gmail.com>
2017-02-07 13:20:06 -0600

    add DetectIOSService

---
+++ app/assets/javascripts/app/services/DetectIOSService.js
@@ -0,0 +1,27 @@
+function DetectIOSService() {
+  
+  this.iOS = function() {
+
+    var iDevices = [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod'
+    ];
+
+    if (!!navigator.platform) {
+      while (iDevices.length) {
+        if (navigator.platform === iDevices.pop()){ return true; }
+      }
+    }
+
+    return false;
+  }
+
+}
+
+angular
+  .module('app')
+  .service('DetectIOSService', DetectIOSService);
