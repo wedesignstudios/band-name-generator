0078e2f1fd75471d752b92ed1e6029b6d2bbbea5
wedesignstudios <deelden@gmail.com>
2017-02-23 11:07:01 -0600

    rename/rollback to previous code in config/initializers/rack-cors.rb

---
+++ config/initializers/rack-cors.rb
@@ -0,0 +1,18 @@
+## Configure Rack CORS Middleware, so that CloudFront can serve our assets.
+## See https://github.com/cyu/rack-cors
+
+if defined? Rack::Cors
+  Rails.configuration.middleware.insert_before 0, Rack::Cors do
+    allow do
+        origins %w[
+            https://mybanderoo.com
+             http://mybanderoo.com
+            https://www.mybanderoo.com
+             http://www.mybanderoo.com
+            https://mybanderoo.herokuapp.com/
+             http://mybanderoo.herokuapp.com/
+        ]
+        resource '/assets/*'
+    end
+  end
+end
